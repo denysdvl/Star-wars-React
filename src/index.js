b01100ee0b2f5d82127cbc6c6cb3b0ffc031c089
Denys Zhorov <2013dvl@gmail.com>
2019-05-13 18:20:48 +0200

    add class SwapiService and add gets people, planets, starships

--- src/index.js
+++ src/index.js
@@ -1,12 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+class SwapiService {
+    constructor() {
+        const __apiBase = "https://swapi.co/api";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+        async function getResource(url) {
+            const res = await fetch(`${__apiBase}${url}`);
+            if (!res.ok) {
+                throw new Error(`Could not fetch ${__apiBase}${url}` +
+                    ` received ${res.status}`);
+            }
+            return await res.json();
+        }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+        this.getAllPeople = async () => {
+            const res = await getResource(`/people/`);
+            return res.results;
+        };
+        this.getPerson = (id) => {
+            return getResource(`/people/${id}`);
+        };
+        this.getAllPlanets = async () => {
+            const res = await getResource(`/planets/`);
+            return res.results;
+        };
+        this.getPlanets = (id) => {
+            return getResource(`/planets/${id}`);
+        };
+        this.getAllStarships = async () => {
+            const res = await getResource(`/starships/`);
+            return res.results;
+        };
+        this.getStarships = (id) => {
+            return getResource(`/starships/${id}`);
+        };
+
+    }
+}
+const swapi = new SwapiService();
+
+// swapi.getAllPeople().then((people) => {
+//     people.forEach(p => {
+//         console.log(p.name);
+//     });
+// });
+// swapi.getPerson(4).then((p) => {
+//     console.log(p.name);
+// });
+
+// swapi.getAllPlanets().then((people) => {
+//     people.forEach(p => {
+//         console.log(p.name);
+//     });
+// });
+// swapi.getPlanets(5).then((p) => {
+//     console.log(p.name, "23");
+// });
+
+swapi.getAllStarships().then((people) => {
+    people.forEach(p => {
+        console.log(p.name);
+    });
+});
+swapi.getStarships(5).then((p) => {
+    console.log(p.name, "23");
+});

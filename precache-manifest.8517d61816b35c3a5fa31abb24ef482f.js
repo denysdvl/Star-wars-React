41110c51cd2c3316b98f877670e1706fd74c10db
Denys Zhorov <2013dvl@gmail.com>
2019-07-05 21:42:18 +0200

    Updates

---
+++ precache-manifest.8517d61816b35c3a5fa31abb24ef482f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de3589fe8a933619c3729fea531a530d",
+    "url": "/Star-wars-React/index.html"
+  },
+  {
+    "revision": "ee6c8467e4bc803db84a",
+    "url": "/Star-wars-React/static/css/main.dddf51e5.chunk.css"
+  },
+  {
+    "revision": "3be3b69c57b21872a79e",
+    "url": "/Star-wars-React/static/js/2.98822ef9.chunk.js"
+  },
+  {
+    "revision": "ee6c8467e4bc803db84a",
+    "url": "/Star-wars-React/static/js/main.98718dc3.chunk.js"
+  },
+  {
+    "revision": "74134ca5c2ab2a1aa1c8",
+    "url": "/Star-wars-React/static/js/runtime~main.09bb0ef8.js"
+  },
+  {
+    "revision": "77ab57fb978384620c6a3e46076c10b5",
+    "url": "/Star-wars-React/static/media/error-404.77ab57fb.gif"
+  },
+  {
+    "revision": "acc87b79d9bd08f789980c4f2058e715",
+    "url": "/Star-wars-React/static/media/error-indicator.acc87b79.png"
+  }
+]);
